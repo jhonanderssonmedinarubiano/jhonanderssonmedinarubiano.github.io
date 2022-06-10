450fef76b123e36f6e7804e6e7ce919b246eefdd
jhonanderssonmedinarubiano <107211225+jhonanderssonmedinarubiano@users.noreply.github.com>
2022-06-09 20:10:05 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,82 @@
+function circulo(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("circulo")
+}
+
+function huevo(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("huevo")
+}
+
+function rombo(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("rombo")
+}
+
+function rectanguloH(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("rectanguloH")
+}
+
+function rectanguloV(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("rectanguloV")
+}
+
+function izquierda(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("izquierda")
+}
+
+function derecha(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("derecha")
+}
+
+function arriba(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("arriba")
+}
+
+function abajo(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("abajo")
+}
+
+function diagonal(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("diagonal")
+}
+
+function triangulo(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("triangulo")
+}
+
+function trianguloe(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("trianguloe")
+}
+
+function paralelo(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("paralelo")
+}
+
+function imagen(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("imagen")
+}
+
+function pacman(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("pacman")
+}
+
+function semi(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("semi")
+}
+function cambiarpag(){
+    window.open("file:///C:/Users/APRENDIZ/Desktop/Datos/index.html")
+}
